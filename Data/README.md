e616882f342950bc1b3a5fee4527984e51dd667c
bianhaiyang <bhy22@mails.tsinghua.edu.cn>
2024-01-06 10:00:37 +0800

    clear

--- Data/README.md
+++
@@ -1,5 +0,0 @@
-put the data here
-
-data used in the tutorials:
-cell type annotation: liver_test.h5ad
-integration: lung_atlas.h5ad
--- scMulan/__init__.py
+++
@@ -1,6 +0,0 @@
-from .model.model import MulanConfig, scMulanModel
-from .model.model_kvcache import cellGPTModel_kv
-from .utils.hf_tokenizer import scMulanTokenizer
-from .scMulan import model_inference
-from .reference.GeneSymbolUniform.pyGSUni import  GeneSymbolUniform
-from .utils.utils import cell_type_smoothing, visualize_selected_cell_types
